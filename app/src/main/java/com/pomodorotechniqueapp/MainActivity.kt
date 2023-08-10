f8d93097b1bdcb0dfdc9c3b7afdcab85a3564c58
Turki <Terki3423@gmail.com>
2023-08-10 06:32:08 +0300

    first commit

---
+++ app/src/main/java/com/pomodorotechniqueapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.pomodorotechniqueapp
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.pomodorotechniqueapp.extensions.showToast
+
+class MainActivity : AppCompatActivity() {
+    private val START_TIME_IN_MILLIS: Long = 25 * 60 * 1000
+    private var remainingTime: Long = START_TIME_IN_MILLIS
+    private lateinit var startButton: Button
+    private lateinit var resetTimer: TextView
+    private lateinit var timer: TextView
+    private lateinit var title: TextView
+    private lateinit var myTimer: CountDownTimer
+    private lateinit var progressBar: ProgressBar
+    private var isTimeRunning = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initializeViews()
+
+        startButton.setOnClickListener {
+            if (!isTimeRunning) {
+                title.setText(R.string.keep_going)
+                startTimer()
+                isTimeRunning = true
+            }
+        }
+        resetTimer.setOnClickListener { resetTime() }
+    }
+    private fun initializeViews() {
+        startButton = findViewById(R.id.start_button)
+        resetTimer = findViewById(R.id.reset)
+        timer = findViewById(R.id.timer)
+        title = findViewById(R.id.pomodoro_title)
+        progressBar = findViewById(R.id.progressBar)
+    }
+
+    private fun startTimer(time: Long = START_TIME_IN_MILLIS) {
+        myTimer = object : CountDownTimer(time, 1000) {
+            override fun onTick(timeLeftInMillis: Long) {
+                remainingTime = timeLeftInMillis
+                updateTimer()
+                progressBar.progress = calculateProgressBar()
+            }
+
+            private fun calculateProgressBar() =
+                (remainingTime.toDouble() / START_TIME_IN_MILLIS.toDouble() * 100).toInt()
+
+            override fun onFinish() {
+                showToast("take 5 minutes break")
+                resetTime()
+            }
+        }.start()
+    }
+
+    private fun resetTime() {
+        myTimer.cancel()
+        timer.setText(R.string.default_time)
+        title.setText(R.string.take_pomodoro)
+        isTimeRunning = false
+        progressBar.progress = 100
+
+    }
+
+    private fun updateTimer() {
+        //convert milli seconds to minutes // minutes = millisecond / 1000 / 60
+        val minutes = remainingTime.div(1000).div(60)
+        //convert milli seconds to seconds // seconds = millisecond / 1000 % 60
+        val seconds = remainingTime.div(1000) % 60
+
+        val formattedTime = String.format("%02d:%02d", minutes, seconds)
+        timer.text = formattedTime
+    }
+}
---
+++ app/src/main/java/com/pomodorotechniqueapp/extensions/extensions.kt
@@ -0,0 +1,10 @@
+package com.pomodorotechniqueapp.extensions
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message,length).show()
+}
+
+
